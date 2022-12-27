38addce2150ec48f5cc6f26fb0008d234b817f1d
Amcmike3 <amcmichael3@gmail.com>
2022-12-26 23:21:35 -0700

    add notes, stubbed out merge sort

--- src/com/mcmichael/concepts/sorting/Sorting.java
+++ src/com/mcmichael/concepts/sorting/Sorting.java
@@ -10,18 +10,64 @@ public class Sorting {
 	
 	private void run() {
 		int[] placeholder = randIntArr(10);
-//		unsorted = help.copyArray(placeholder);
-		int[] unsorted = {10,9,8,7,6,5,4,3,2,1};
+		int[] unsorted = help.copyArray(placeholder);
+//		int[] unsorted = {10,9,8,7,6,5,4,3,2,1};
 		bubbleSort(unsorted);
-//		int[] unsorted = help.copyArray(placeholder);
-		unsorted = help.worstCase();
+		unsorted = help.copyArray(placeholder);
+//		unsorted = help.worstCase();
 		selectionSort(unsorted);
-//		unsorted = help.copyArray(placeholder);
-		unsorted = help.worstCase();
+		unsorted = help.copyArray(placeholder);
+//		unsorted = help.worstCase();
 		insertionSort(unsorted);
+		unsorted = help.copyArray(placeholder);
+//		unsorted = help.worstCase();
+		shellSort(unsorted);
 		
 		
 	}
+	/*
+	 * separate original list into many smaller lists and then use insertion sort on the smaller lists
+	 * this creates a list that is nearly sorted before a final insertion sort is performed on the nearly sorted list.
+
+	 */
+	private void shellSort(int [] unsorted) {
+		System.out.println("Shell sort start");
+		help.printList(unsorted); 
+		int index = 0;
+		//start shell sort necessary code:
+		int increment = unsorted.length / 2;
+		while (increment >= 1) {
+			for (int start = 0; start < increment; start++) {
+				index += shellInsertionSort(unsorted, start, increment);
+			}
+			increment = increment /2;
+		}
+		
+		System.out.println("-------------------------------------");
+		help.printList(unsorted);
+		System.out.println("Shell sort end " + index + " iterations");
+		System.out.println("-------------------------------------");
+	}
+	 // this is a modified version of insertion sort made for shell sort
+	// basically use insertion sort for each sublist
+	private int shellInsertionSort(int [] unsorted, int start, int increment) {
+		int index = 0;
+
+		for (int i = start; i < unsorted.length; i += increment) { 
+			for (int j = Math.min(i + increment,unsorted.length -1); j- increment >= 0; j -= increment) {
+				if (unsorted[j] < unsorted[ j - increment]) {
+					index++;
+					help.swap(unsorted, j, j - increment);
+					help.printList(unsorted);
+				} else {
+					break;
+				}
+			}
+		}
+		return index;
+		
+	}
+	
 	/*
 	 * iterate over array comparing the element just outside the sorted portion. 
 	 * if the element just outside is less than the last element in the sorted portion, move it over until it is in the correct position.
