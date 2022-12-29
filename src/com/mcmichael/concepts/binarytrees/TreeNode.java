23dcaf417527e7c3296d244de64845ff291dbd42
Amcmike3 <amcmichael3@gmail.com>
2022-12-29 12:07:10 -0700

    add binary tree and treenode class

---
+++ src/com/mcmichael/concepts/binarytrees/TreeNode.java
@@ -0,0 +1,34 @@
+package com.mcmichael.concepts.binarytrees;
+
+public class TreeNode<T> {
+
+	private T data;
+	private TreeNode<T> leftChild;
+	private TreeNode<T> rightChild;
+	
+	public TreeNode(T data) {
+		this.data = data;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public TreeNode<T> getLeftChild() {
+		return leftChild;
+	}
+
+	public void setLeftChild(TreeNode<T> leftChild) {
+		this.leftChild = leftChild;
+	}
+
+	public TreeNode<T> getRightChild() {
+		return rightChild;
+	}
+
+	public void setRightChild(TreeNode<T> rightChild) {
+		this.rightChild = rightChild;
+	}
+
+	
+}
