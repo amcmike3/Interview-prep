85e42591a1c17ef77a6f0aa57489c0df46728b28
Amcmike3 <amcmichael3@gmail.com>
2023-01-02 16:42:34 -0700

    add Depth first Traversal and stud out pre order, in order, and post order DFT methods

--- src/com/mcmichael/concepts/binarytrees/DepthFirstTraversal.java
+++ src/com/mcmichael/concepts/binarytrees/DepthFirstTraversal.java
@@ -2,4 +2,12 @@ package com.mcmichael.concepts.binarytrees;
 
 public class DepthFirstTraversal {
 
+	public void preOrder() {
+		
+	}
+	public void postOrder() {
+	}
+	public void inOrder() {
+		
+	}
 }
