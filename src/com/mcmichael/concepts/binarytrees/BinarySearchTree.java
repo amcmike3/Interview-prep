09d1952987308e0482dc2a4d2c7acdb9886a0c1d
Amcmike3 <amcmichael3@gmail.com>
2023-01-03 08:37:38 -0700

    create main to test methods and delete myLookup

--- src/com/mcmichael/concepts/binarytrees/BinarySearchTree.java
+++ src/com/mcmichael/concepts/binarytrees/BinarySearchTree.java
@@ -1,6 +1,19 @@
 package com.mcmichael.concepts.binarytrees;
 
 public class BinarySearchTree {
+	public static void main(String[] args) {
+		TreeNode<Integer> head = new TreeNode<Integer>(8);
+		head.setLeftChild( new TreeNode<Integer>(6));
+		head.getLeftChild().setLeftChild(new TreeNode<Integer>(4));
+		head.getLeftChild().setRightChild(new TreeNode<Integer>(7));
+		head.setRightChild(new TreeNode<Integer>(14));
+		head.getRightChild().setRightChild(new TreeNode<Integer>(16));
+		head.getRightChild().getRightChild().setRightChild(new TreeNode<Integer>(18));
+		
+		System.out.println(lookup(head, 14).getData());
+	}
+	
+
 	public static TreeNode<Integer> insert(TreeNode<Integer> head, TreeNode<Integer> node){
 		if (head == null) {
 			return node;
@@ -14,19 +27,7 @@ public class BinarySearchTree {
 		
 		return head;
 	}
-	public static TreeNode<Integer> myLookUp(TreeNode<Integer> head, Integer data){
-		TreeNode<Integer> ans = null;
-		while (head.getData() != null || head.getData() != data) {
-			if(head.getData() <= data) {
-				head = head.getLeftChild();
-			} else {
-				head = head.getRightChild();
-			}
-		}
-		
-		return ans;
-		
-	}
+
 	public static TreeNode<Integer> lookup(TreeNode<Integer> head, int data){
 		if (head == null) {
 			return null;
