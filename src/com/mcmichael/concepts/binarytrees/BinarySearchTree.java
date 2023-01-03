b155d354bc9573e8fc776447949d83ec0da5debb
Amcmike3 <amcmichael3@gmail.com>
2023-01-03 08:39:54 -0700

    in main test method insert

--- src/com/mcmichael/concepts/binarytrees/BinarySearchTree.java
+++ src/com/mcmichael/concepts/binarytrees/BinarySearchTree.java
@@ -3,12 +3,12 @@ package com.mcmichael.concepts.binarytrees;
 public class BinarySearchTree {
 	public static void main(String[] args) {
 		TreeNode<Integer> head = new TreeNode<Integer>(8);
-		head.setLeftChild( new TreeNode<Integer>(6));
-		head.getLeftChild().setLeftChild(new TreeNode<Integer>(4));
-		head.getLeftChild().setRightChild(new TreeNode<Integer>(7));
-		head.setRightChild(new TreeNode<Integer>(14));
-		head.getRightChild().setRightChild(new TreeNode<Integer>(16));
-		head.getRightChild().getRightChild().setRightChild(new TreeNode<Integer>(18));
+		insert(head, new TreeNode<Integer>(6));
+		insert(head, new TreeNode<Integer>(4));
+		insert(head, new TreeNode<Integer>(7));
+		insert(head, new TreeNode<Integer>(14));
+		insert(head, new TreeNode<Integer>(16));
+		insert(head, new TreeNode<Integer>(18));
 		
 		System.out.println(lookup(head, 14).getData());
 	}
