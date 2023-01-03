5dc859543860b3d73f37c64f1f2e3fe109cbfbce
Amcmike3 <amcmichael3@gmail.com>
2023-01-02 21:33:30 -0700

    implement insert method for BinarySearchTree

--- src/com/mcmichael/concepts/binarytrees/BinarySearchTree.java
+++ src/com/mcmichael/concepts/binarytrees/BinarySearchTree.java
@@ -1,5 +1,18 @@
 package com.mcmichael.concepts.binarytrees;
 
 public class BinarySearchTree {
+	public static TreeNode<Integer> insert(TreeNode<Integer> head, TreeNode<Integer> node){
+		if (head == null) {
+			return node;
+		}
+		
+		if (node.getData() <= head.getData()) {
+			head.setLeftChild(insert(head.getLeftChild(), node));
+		} else {
+			head.setRightChild(insert(head.getRightChild(), node));
+		}
+		
+		return head;
+	}
 
 }
