23e0bcc51c0e4c9a99968bb1cd6a0ae185183930
Amcmike3 <amcmichael3@gmail.com>
2023-01-03 08:15:33 -0700

    add binary search tree lookup method

--- src/com/mcmichael/concepts/binarytrees/BinarySearchTree.java
+++ src/com/mcmichael/concepts/binarytrees/BinarySearchTree.java
@@ -14,5 +14,33 @@ public class BinarySearchTree {
 		
 		return head;
 	}
+	public static TreeNode<Integer> myLookUp(TreeNode<Integer> head, Integer data){
+		TreeNode<Integer> ans = null;
+		while (head.getData() != null || head.getData() != data) {
+			if(head.getData() <= data) {
+				head = head.getLeftChild();
+			} else {
+				head = head.getRightChild();
+			}
+		}
+		
+		return ans;
+		
+	}
+	public static TreeNode<Integer> lookup(TreeNode<Integer> head, int data){
+		if (head == null) {
+			return null;
+		}
+		if (head.getData() == data) {
+			return head;
+		}
+		
+		if (data <= head.getData()) {
+			return lookup(head.getLeftChild(), data);
+		} else {
+			return lookup(head.getRightChild(), data);
+		}
+		
+	}
 
 }
