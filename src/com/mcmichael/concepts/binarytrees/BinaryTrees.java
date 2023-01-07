22366dc39fb989e92ad7b2cee82c38aaab912f56
Amcmike3 <amcmichael3@gmail.com>
2023-01-07 16:57:51 -0700

    minimumValueInBinarySearchTree in BinaryTree

--- src/com/mcmichael/concepts/binarytrees/BinaryTrees.java
+++ src/com/mcmichael/concepts/binarytrees/BinaryTrees.java
@@ -1,5 +1,19 @@
 package com.mcmichael.concepts.binarytrees;
 
 public class BinaryTrees {
+	
+	
+	private Integer minimumValueInBinarySearchTree(TreeNode<Integer> head){
+		if (head == null) {
+			return Integer.MIN_VALUE;
+		}
+		
+		if (head.getLeftChild() == null) {
+			return head.getData();
+		}
+		
+		
+		return minimumValueInBinarySearchTree(head.getLeftChild());
+	}
 
 }
