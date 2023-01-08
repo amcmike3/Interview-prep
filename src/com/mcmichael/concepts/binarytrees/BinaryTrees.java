41a813fa227918fd22168eab9e0c20ac3d506d80
Amcmike3 <amcmichael3@gmail.com>
2023-01-07 21:05:24 -0700

    add countTrees to BinaryTrees

--- src/com/mcmichael/concepts/binarytrees/BinaryTrees.java
+++ src/com/mcmichael/concepts/binarytrees/BinaryTrees.java
@@ -2,7 +2,7 @@ package com.mcmichael.concepts.binarytrees;
 
 public class BinaryTrees {
 	
-	
+	// find the smallest value within a Binary Search Tree
 	private Integer minimumValueInBinarySearchTree(TreeNode<Integer> head){
 		if (head == null) {
 			return Integer.MIN_VALUE;
@@ -16,6 +16,7 @@ public class BinaryTrees {
 		return minimumValueInBinarySearchTree(head.getLeftChild());
 	}
 	
+//	find the deepest level of the given tree
 	private Integer maxDepthBinaryTree(TreeNode<Integer> head){
 		if(head == null) {
 			return 0;
@@ -32,6 +33,7 @@ public class BinaryTrees {
 		return leftDepth >= rightDepth ? leftDepth : rightDepth;
 	}
 	
+	//reverse all node in a given tree so tree become mirror of the original
 	private void mirrorTree(TreeNode<Integer> head) {
 		if (head == null ) {
 			return;
@@ -46,6 +48,22 @@ public class BinaryTrees {
 		head.setRightChild(temp);
 		
 		
+	}
+	
+	//count the total number of structurally unique trees can be created given a number of nodes
+	private int countTrees(int numNodes) {
+		if (numNodes <= 1 ) {
+			return 1;
+		}
+		int sum = 0;
+		for (int i = 1; i <= numNodes; i++) {
+			int countLeftTrees = countTrees(i - 1);
+			int countRightTrees = countTrees(numNodes - 1);
+			sum += countLeftTrees * countRightTrees;
+			
+		}
+		
+		return sum;
 	}
 
 }
