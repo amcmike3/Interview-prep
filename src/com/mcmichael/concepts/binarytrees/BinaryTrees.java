6fb27470071dfe019aa909898857c18620b1664c
Amcmike3 <amcmichael3@gmail.com>
2023-01-07 18:19:23 -0700

    add mirrorTree to BinaryTrees

--- src/com/mcmichael/concepts/binarytrees/BinaryTrees.java
+++ src/com/mcmichael/concepts/binarytrees/BinaryTrees.java
@@ -15,5 +15,37 @@ public class BinaryTrees {
 		
 		return minimumValueInBinarySearchTree(head.getLeftChild());
 	}
+	
+	private Integer maxDepthBinaryTree(TreeNode<Integer> head){
+		if(head == null) {
+			return 0;
+		}
+	
+		
+		if (head.getLeftChild() == null && head.getRightChild() == null) {
+			return 0;
+		}
+
+		int leftDepth = 1 + maxDepthBinaryTree(head);
+		int rightDepth = 1 + maxDepthBinaryTree(head);
+		
+		return leftDepth >= rightDepth ? leftDepth : rightDepth;
+	}
+	
+	private void mirrorTree(TreeNode<Integer> head) {
+		if (head == null ) {
+			return;
+		}
+		
+		
+		mirrorTree(head.getLeftChild());
+		mirrorTree(head.getRightChild());
+		
+		TreeNode<Integer> temp = head.getLeftChild();
+		head.setLeftChild(head.getRightChild());
+		head.setRightChild(temp);
+		
+		
+	}
 
 }
