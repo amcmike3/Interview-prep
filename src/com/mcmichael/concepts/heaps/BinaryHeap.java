4ab1006dbeae0d296d3f8a1ac46aa106e8ac72cf
Amcmike3 <amcmichael3@gmail.com>
2023-01-15 19:13:25 -0700

    add binary heap notes

--- src/com/mcmichael/concepts/heaps/BinaryHeap.java
+++ src/com/mcmichael/concepts/heaps/BinaryHeap.java
@@ -5,7 +5,8 @@ public class BinaryHeap {
 	 * binary heaps need to have pointers to its parent as well as its children.
 	 * a better way than storing so many objects with inter-relations to handle this is mathematically.
 	 * A Binary heap can be stored inside of an array with the mathematical formulas that follow
-	 * to represent the relationships. i represents the index of the current node in the tree.
+	 * to represent the relationships. 
+	 * i represents the index of the current node in the tree.
 	 * to get the left child: 2*i + 1;
 	 * to get the right child: 2*i + 2;
 	 * to get the parent: (i-1)/2 ; 
