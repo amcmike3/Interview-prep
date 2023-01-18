e995f30d413b19bc98509699e44ab5790349dd09
Amcmike3 <amcmichael3@gmail.com>
2023-01-18 09:00:34 -0700

    add minimum heap constructors

--- src/com/mcmichael/concepts/heaps/MinHeap.java
+++ src/com/mcmichael/concepts/heaps/MinHeap.java
@@ -1,5 +1,15 @@
 package com.mcmichael.concepts.heaps;
 
-public class MinHeap {
+//minimum heap  is a heap which all nodes below any particular node are greater. 
+// in other words the smallest values are highest in the tree.
+public class MinHeap<T extends Comparable> extends BinaryHeap<T> {
+
+	public MinHeap(Class<T> clazz) {
+		super(clazz);
+	}
+	
+	public MinHeap(Class<T> clazz, int size ) {
+		super(clazz, size);
+	}
 
 }
